b4a853116a09c1373b5fa1f87d3e2b6d1639bab1
Logan Glasco <lglasco81@gmail.com>
2019-10-16 20:57:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,15 +1,14 @@
 function basicTeenager(age) {
 if (age <=13 && age >= 19)
 return "You are a teenager"
- 
+
 function teenager(age) {
 
 
 function ageChecker(age) {
 
-    
+
 function ternaryTeenager(age) {
 
 
 function switchAge(age) {
-
